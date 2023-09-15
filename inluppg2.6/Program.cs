7d62a05c1e01b8861630894b17cd236aa4c259a5
emsi06001 <emsi06001@E-PW0234N3>
2023-09-15 09:06:35 +0200

    Add project files.

---
+++ inluppg2.6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace inluppg2_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv en mening med tre ord och punkt i slutet");
+            string mening = Console.ReadLine();
+            int mellanslag1 = mening.IndexOf(' ');
+            int mellanslag2 = mening.IndexOf(' ',mellanslag1 + 1);
+            int punkt = mening.IndexOf('.');
+
+            Console.WriteLine($"{mening[(mellanslag2+1)..punkt]}{mening[mellanslag1..mellanslag2]} {mening[..mellanslag1]}.");
+        }
+    }
+}
